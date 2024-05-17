93f3e31d2e4280d8a1513e81b9733bd8ee5201de
Flopendop <dominik.lauzon@gmx.de>
2024-05-17 14:48:33 +0200

    camera

---
+++ Camera.py
@@ -0,0 +1,16 @@
+from djitellopy import tello
+import cv2
+
+tello = tello.Tello()
+tello.connect()
+tello.streamon()
+
+print(tello.get_battery())
+
+
+while True:
+    img = tello.get_frame_read().frame
+    img = cv2.resize(img, (500,500))
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    cv2.imshow("Bild", img)
+    cv2.waitKey(1)
