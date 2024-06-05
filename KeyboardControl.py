35ecaf130ea10f1c11dca9db44aac50c4a768d5c
Tobias <tobias.uhle@t-online.de>
2024-06-05 20:17:10 +0200

    Changed main

--- KeyboardControl.py
+++ KeyboardControl.py
@@ -7,38 +7,38 @@ def keyboardControl():
 
     # Tastenbelegung und -abfrage
 
-    eingabe, ud, yv, fb, rl, start, land = 0, 0, 0, 0, 0, 0, 0
+    eingabe, ud, yv, fb, rl, start, land = False, 0, 0, 0, 0, 0, 0
     gedrueckt = pygame.key.get_pressed()
     if gedrueckt[pygame.K_UP]:
         ud = 100
-        eingabe = 1
+        eingabe = True
     elif gedrueckt[pygame.K_DOWN]:
         ud = -100
-        eingabe = 1
+        eingabe = True
     if gedrueckt[pygame.K_RIGHT]:
         yv = 100
-        eingabe = 1
+        eingabe = True
     elif gedrueckt[pygame.K_LEFT]:
         yv = -100
-        eingabe = 1
+        eingabe = True
     if gedrueckt[pygame.K_w]:
         fb = 100
-        eingabe = 1
+        eingabe = True
     elif gedrueckt[pygame.K_s]:
         fb = -100
-        eingabe = 1
+        eingabe = True
     if gedrueckt[pygame.K_d]:
         rl = 100
-        eingabe = 1
+        eingabe = True
     elif gedrueckt[pygame.K_a]:
         rl = -100
-        eingabe = 1
+        eingabe = True
     if gedrueckt[pygame.K_e]:
         start = 1
-        eingabe = 1
+        eingabe = True
     elif gedrueckt[pygame.K_q]:
         land = 1
-        eingabe = 1
+        eingabe = True
     return[eingabe, ud, yv, fb, rl, start, land, 0, 0]
     
     #tello.send_rc_control(rl, fb, ud, yv)
--- main.py
+++ main.py
@@ -6,15 +6,21 @@ from djitellopy import tello
 import pygame
 import sys
 import KeyboardControl
+import XBoxControl
+import os
+import time
+from inputs import get_gamepad
 #-------------------------------------------------------------------------------------
 # VARIABLEN
 #-------------------------------------------------------------------------------------
 
 # Großgeschrieben = Feste Variablen
 DROHNE_AKTIV = False
-EINGABEMODUS = "xbox" # xbox oder key
+#EINGABEMODUS = "xbox" # xbox oder key
 
 running = True
+
+controller = XBoxControl.XboxController()
 
 #-------------------------------------------------------------------------------------
 # METHODEN
@@ -27,6 +33,10 @@ def checkForExit():
             tello.land()
             sys.exit()
 
+
+# Ausgabefenster clearen:
+
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear') #direkt für Windows und Linux implementiert
 
 #-------------------------------------------------------------------------------------
 
@@ -45,21 +55,44 @@ pygame.display.set_caption("Drohnensteuerung")
 # HAUPTSCHLEIFE
 #-------------------------------------------------------------------------------------
 
+
+
 while running:
+
+    clear()
 
     checkForExit()
 
-    # Steuerungsstandard: [Eingabe gegeben, Hoch + Runter, Drehen Uhrzeigersinn + Gegenuhrzeigersinn, Vorwärts + Rückwärts, Rechts + Links, starten, landen, Button3, Button4]
-    # [0 und 1, -100 bis 100, -100 bis 100, -100 bis 100, -100 bis 100, 0 und 1, 0 und 1, 0 und 1, 0 und 1]
 
-    if EINGABEMODUS == "key":
+    # Steuerungsstandard: [Eingabe gegeben, Hoch + Runter, Drehen Uhrzeigersinn + Gegenuhrzeigersinn, Vorwärts + Rückwärts, Rechts + Links, starten, landen, Button3, Button4]
+    # [False oder True, -100 bis 100, -100 bis 100, -100 bis 100, -100 bis 100, 0 und 1, 0 und 1, 0 und 1, 0 und 1]
+
+    #if EINGABEMODUS == "key":
         #Daten von Tastatur
-        KeyboardControl.keyboardControl()
+    KeyboardDaten = KeyboardControl.keyboardControl()
+
     #else:
         #Daten von Controller
+    
+    
+    ControllerDaten = controller.read()
 
+    SteuerungsDaten = [0,0,0,0,0,0,0,0]
+    
+    if KeyboardDaten[0] == 1:
+        print("Nutze Keyboard")
+        SteuerungsDaten = KeyboardDaten[1:9]
+    else:
+        print("Nutze Controller")
+        #print(ControllerDaten)
+        SteuerungsDaten = ControllerDaten[1:9]
+    
+    print(SteuerungsDaten)
 
     # Hier die Daten in Bewegungsbefehle
         
     #pygame.display.update()
     #clock.tick(60)
+
+    time.sleep(0.01)
+    
