6a18fdf4bc477f6dfe7297ab67ff7c014519264b
Flopendop <dominik.lauzon@gmx.de>
2024-06-07 14:19:56 +0200

    neu

--- Camera.py
+++ Camera.py
@@ -2,7 +2,7 @@ from djitellopy import tello
 import cv2
 import pygame
 from PIL import Image
-
+import dronecomms
 
 pygame.init()
 screen = pygame.display.set_mode((1500, 1000))
@@ -13,8 +13,6 @@ tello = tello.Tello()
 tello.connect()
 tello.streamon()
 
-print(tello.get_battery())
-
 #pfeil = pygame.image.load("pfeil.jpg")
 
 while running:
@@ -23,13 +21,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    img = tello.get_frame_read().frame
-    img = pygame.image.frombuffer(img.tostring(), img.shape[1::-1],"RGB") # Formatierung
+    img = dronecomms.dronecomms.getImage()
     #img = cv2.resize(img, (500,500))
     #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     #cv2.imshow("Bild", img)
     #cv2.waitKey(1)
-
+    
     #img = cv2.resize(img,(1920,1080))
     
     screen.blit(img, (0,0))
